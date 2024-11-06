a14e9404e795495014896768ff33b9c9fde33909
codud0905 <codud0905@g.skku.edu>
2024-11-06 19:48:43 +0900

    main.py

--- main.py
+++ main.py
@@ -2,10 +2,28 @@ from typing import List
 
 # even_list 함수 구현
 def even_list(int_list: List[int]) -> List[int]:
+    """
+    Determines if a number is even and return an even list.
+    
+    Args:
+        int_list: A list of integers.
+        
+    Returns:
+        A list of even integers.
+    """
     return [x for x in int_list if x % 2 == 0]
 
 # sum_of_squares_of_even 함수 구현
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:
+    """
+    Computes the sum of the squares of all even numbers in the list.
+    
+    Args:
+        even_int_list: A list of even integers.
+        
+    Returns:
+        The sum of the squares of all even numbers in the list.
+    """
     return sum(x ** 2 for x in even_int_list)
 
 # Main function
